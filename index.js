5753707dfcd1fd639452271b2781a9ac2b7e2484
Andrew Nesbitt <andrewnez@gmail.com>
2022-02-11 16:04:11 +0000

    hello world

---
+++ index.js
@@ -0,0 +1,41 @@
+const http = require('http')
+const fetch = require('node-fetch');
+const url = require('url');
+// const AbortController = require('abort-controller'); // TODO timeout after 30 seconds
+var crypto = require('crypto');
+
+async function hash(algo, url) {
+  // TODO support go modules hashing algo
+
+  var download = await fetch(url) // TODO handle failure to load url gracefully
+  const hash = crypto.createHash(algo).update(await download.buffer()).digest('base64');
+  return {algo: hash}
+}
+
+const handler = async function (req, res) {
+  var parsedUrl = url.parse(req.url,true)
+  var query = parsedUrl.query
+
+  if (parsedUrl.pathname == '/hash') {
+    if(query.url){
+      var result = await hash(query.algorithm, query.url)
+    } else {
+      var result = {error: 'invalid parameters'}
+    }
+  } else {
+    var result = {error: 'incorrect path'}
+  } 
+
+  res.setHeader('Content-Type', 'application/json');
+
+  if (result.hash){
+    res.writeHead(200);
+  } else {
+    res.writeHead(500);
+  }
+
+  res.end(JSON.stringify(result));
+}
+
+const server = http.createServer(handler);
+server.listen(process.env.PORT || 8080);
