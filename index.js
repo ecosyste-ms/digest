4fe21bad94ebebf0dcc94d64c0b7c41643721481
Andrew Nesbitt <andrewnez@gmail.com>
2022-03-21 10:39:28 +0000

    rename to digest

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ const url = require('url');
 // const AbortController = require('abort-controller'); // TODO timeout after 30 seconds
 var crypto = require('crypto');
 
-async function hash(algorithm, url) {
+async function digest(algorithm, url) {
   // TODO support go modules hashing algo
 
   var download = await fetch(url) // TODO handle failure to load url gracefully
-  const hash = crypto.createHash(algorithm).update(await download.buffer()).digest('base64');
-  return {algorithm, hash}
+  const digest = crypto.createHash(algorithm).update(await download.buffer()).digest('base64');
+  return {algorithm, digest}
 }
 
 const handler = async function (req, res) {
   var parsedUrl = url.parse(req.url,true)
   var query = parsedUrl.query
 
-  if (parsedUrl.pathname == '/hash') {
+  if (parsedUrl.pathname == '/digest') {
     if(query.url){
-      var result = await hash(query.algorithm, query.url)
+      var result = await digest(query.algorithm, query.url)
     } else {
       var result = {error: 'invalid parameters'}
     }
@@ -28,7 +28,7 @@ const handler = async function (req, res) {
 
   res.setHeader('Content-Type', 'application/json');
 
-  if (result.hash){
+  if (result.digest){
     res.writeHead(200);
   } else {
     res.writeHead(500);
