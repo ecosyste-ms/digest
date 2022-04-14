9f301792388e75b0890c0f21656b4ee1d85f5bd6
Andrew Nesbitt <andrewnez@gmail.com>
2022-04-14 11:13:41 +0100

    Homepage

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const http = require('http')
+const fs = require('fs')
 const fetch = require('node-fetch');
 const url = require('url');
 // const AbortController = require('abort-controller'); // TODO timeout after 30 seconds
 var crypto = require('crypto');
 
 async function digest(algorithm, url) {
-  // TODO support go modules hashing algo
+  try{
+    algorithm = (typeof algorithm !== 'undefined') ?  algorithm : 'sha256'
+    // TODO support go modules hashing algo
 
-  var download = await fetch(url) // TODO handle failure to load url gracefully
-  const digest = crypto.createHash(algorithm).update(await download.buffer()).digest('base64');
-  return {algorithm, digest}
+    var download = await fetch(url) // TODO handle failure to load url gracefully
+    const digest = crypto.createHash(algorithm).update(await download.buffer()).digest('base64');
+    return {algorithm, digest, url}
+  } catch {
+    return {error: 'invalid url'}
+  }
 }
 
 const handler = async function (req, res) {
@@ -20,21 +26,28 @@ const handler = async function (req, res) {
     if(query.url){
       var result = await digest(query.algorithm, query.url)
     } else {
-      var result = {error: 'invalid parameters'}
+      var result = {error: 'invalid parameters, url must be provided'}
     }
+    res.setHeader('Content-Type', 'application/json');
+
+    if (result.digest){
+      res.writeHead(200);
+    } else {
+      res.writeHead(500);
+    }
+
+    res.end(JSON.stringify(result));
+  } else if (parsedUrl.pathname == '/algorithms') {
+    var algorithms = crypto.getHashes();
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(algorithms));
+  } else if (parsedUrl.pathname == '/') {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    fs.createReadStream('index.html').pipe(res)
   } else {
-    var result = {error: 'incorrect path'}
+    res.writeHead(404);
+    res.end('404')
   } 
-
-  res.setHeader('Content-Type', 'application/json');
-
-  if (result.digest){
-    res.writeHead(200);
-  } else {
-    res.writeHead(500);
-  }
-
-  res.end(JSON.stringify(result));
 }
 
 const server = http.createServer(handler);
