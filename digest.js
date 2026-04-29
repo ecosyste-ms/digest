ba09191c4dca72e490cfddb07b2b48adb05f5686
AgentGoose32 <agentgoose32@gmail.com>
2026-04-28 22:36:12 -0400

    Add dirhash support for Go module zips

--- digest.js
+++ digest.js
@@ -1,10 +1,11 @@
 var crypto = require('crypto');
+var fs = require('fs-extra');
+var tmp = require('tmp-promise');
+var { zipDigest } = require('zipdigest');
 
 async function calculateDigest(algorithm, encoding, url) {
     algorithm = (typeof algorithm !== 'undefined') ?  algorithm : 'sha256'
     encoding = (typeof encoding !== 'undefined') ?  encoding : 'hex'
-    // TODO support go modules hashing algo
-
     var controller = new AbortController();
     var timeout = setTimeout(() => controller.abort(), 1000*5);
 
@@ -16,16 +17,42 @@ async function calculateDigest(algorithm, encoding, url) {
 
     // TODO only digest if response is a success (example: 403 with body - https://rubygems.org/downloads/sorbet-static-0.4.5125.gem)
 
+    if (algorithm === 'dirhash') {
+      return await calculateDirhash(url, response);
+    }
+
     var bytes = response.headers.get('content-length');
-    var body = await response.text();
+    var body = Buffer.from(await response.arrayBuffer());
 
     if (!bytes) {
-      bytes = String(Buffer.byteLength(body));
+      bytes = String(body.byteLength);
     }
 
     var digest = crypto.createHash(algorithm).update(body).digest(encoding);
     var sri = `${algorithm}-${digest}`
     return {algorithm, encoding, digest, url, bytes, sri}
+}
+
+async function calculateDirhash(url, response) {
+    var dir = await tmp.dir({ unsafeCleanup: true });
+    var zippath = `${dir.path}/module.zip`;
+
+    try {
+      var body = Buffer.from(await response.arrayBuffer());
+      await fs.writeFile(zippath, body);
+      var digest = `h1:${await zipDigest(zippath)}`;
+
+      return {
+        algorithm: 'dirhash',
+        encoding: 'base64',
+        digest,
+        url,
+        bytes: String(body.byteLength),
+        sri: `dirhash-${digest}`
+      };
+    } finally {
+      await dir.cleanup();
+    }
 }
 
 module.exports = calculateDigest;
